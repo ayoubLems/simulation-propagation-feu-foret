1705b06776fc2f451f3a52997b0354f67188d6e9
ayoubLems <142347510+ayoubLems@users.noreply.github.com>
2025-01-07 22:56:13 +0100

    Add files via upload

---
+++ src/main/java/fr/Ciril/Foret.java
@@ -0,0 +1,123 @@
+package ma.annotation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Foret {
+
+    private int nombreLigne;
+    private int nombreColonne;
+    private double probabilite;
+    private List<List<Grille>> grilles;
+
+    public int getNombreLigne() {
+        return nombreLigne;
+    }
+
+    public void setNombreLigne(int nombreLigne) {
+        this.nombreLigne = nombreLigne;
+    }
+
+    public int getNombreColonne() {
+        return nombreColonne;
+    }
+
+    public void setNombreColonne(int nombreColonne) {
+        this.nombreColonne = nombreColonne;
+    }
+
+    public double getProbabilite() {
+        return probabilite;
+    }
+
+    public void setProbabilite(double probabilite) {
+        this.probabilite = probabilite;
+    }
+
+    public List<List<Grille>> getGrilles() {
+        return grilles;
+    }
+
+    public void setGrilles(List<List<Grille>> grilles) {
+        this.grilles = grilles;
+    }
+
+    public List<Grille> getCasesAdjacentes(Grille grille) {
+        List<Grille> adjacentes = new ArrayList<>();
+        int x = grille.getLigneIndex();
+        int y = grille.getColonneIndex();
+
+        if (x > 0) adjacentes.add(grilles.get(x - 1).get(y));
+        if (x < nombreLigne - 1) adjacentes.add(grilles.get(x + 1).get(y));
+        if (y > 0) adjacentes.add(grilles.get(x).get(y - 1));
+        if (y < nombreColonne - 1) adjacentes.add(grilles.get(x).get(y + 1));
+
+        return adjacentes;
+    }
+
+    public boolean simulerEtape() {
+        List<Grille> nouvellesCasesEnFeu = new ArrayList<>();
+
+        for (var ligne : grilles) {
+            for (var grille : ligne) {
+                if (grille.getEtat() == Etat.EnFeu) {
+                    grille.setEtat(Etat.RemplieDeCendre);
+                    for (Grille adjacente : getCasesAdjacentes(grille)) {
+                        if (adjacente.getEtat() == Etat.JamaisBrulee && Math.random() < probabilite) {
+                            nouvellesCasesEnFeu.add(adjacente);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (Grille grille : nouvellesCasesEnFeu) {
+            grille.setEtat(Etat.EnFeu);
+        }
+        return !nouvellesCasesEnFeu.isEmpty();
+    }
+
+    private Grille getGrille(int ligneIndex, int colonneIndex) {
+        return grilles.get(ligneIndex).get(colonneIndex);
+    }
+
+    public static class Grille {
+        private int ligneIndex;
+        private int colonneIndex;
+        private Etat etat;
+
+        public Grille(int ligneIndex, int colonneIndex, Etat etat) {
+            this.ligneIndex = ligneIndex;
+            this.colonneIndex = colonneIndex;
+            this.etat = etat;
+        }
+
+        public int getLigneIndex() {
+            return ligneIndex;
+        }
+
+        public void setLigneIndex(int ligneIndex) {
+            this.ligneIndex = ligneIndex;
+        }
+
+        public int getColonneIndex() {
+            return colonneIndex;
+        }
+
+        public void setColonneIndex(int colonneIndex) {
+            this.colonneIndex = colonneIndex;
+        }
+
+        public Etat getEtat() {
+            return etat;
+        }
+
+        public void setEtat(Etat etat) {
+            this.etat = etat;
+        }
+    }
+
+    enum Etat {
+        JamaisBrulee, EnFeu, RemplieDeCendre
+    }
+}
---
+++ src/test/java/fr/Ciril/ForetTest.java
@@ -0,0 +1,60 @@
+package ma.annotation;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForetTest extends TestCase {
+
+    private Foret foret;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        foret = new Foret();
+        foret.setNombreLigne(3);
+        foret.setNombreColonne(3);
+        foret.setProbabilite(0.5);
+
+        List<List<Foret.Grille>> grilles = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            List<Foret.Grille> ligne = new ArrayList<>();
+            for (int j = 0; j < 3; j++) {
+                ligne.add(new Foret.Grille(i, j, Foret.Etat.JamaisBrulee));
+            }
+            grilles.add(ligne);
+        }
+
+        grilles.get(1).get(1).setEtat(Foret.Etat.EnFeu);
+        foret.setGrilles(grilles);
+    }
+
+    public void testInitialisation() {
+        assertEquals(3, foret.getNombreLigne());
+        assertEquals(3, foret.getNombreColonne());
+
+        List<List<Foret.Grille>> grilles = foret.getGrilles();
+        assertEquals(Foret.Etat.EnFeu, grilles.get(1).get(1).getEtat());
+        assertEquals(Foret.Etat.JamaisBrulee, grilles.get(0).get(0).getEtat());
+    }
+
+    public void testGetCasesAdjacentes() {
+        Foret.Grille caseCentrale = foret.getGrilles().get(1).get(1);
+        List<Foret.Grille> adjacentes = foret.getCasesAdjacentes(caseCentrale);
+
+        assertNotNull(adjacentes);
+        assertEquals(4, adjacentes.size());
+
+        boolean haut = adjacentes.contains(foret.getGrilles().get(0).get(1));
+        boolean bas = adjacentes.contains(foret.getGrilles().get(2).get(1));
+        boolean gauche = adjacentes.contains(foret.getGrilles().get(1).get(0));
+        boolean droite = adjacentes.contains(foret.getGrilles().get(1).get(2));
+
+        assertTrue(haut);
+        assertTrue(bas);
+        assertTrue(gauche);
+        assertTrue(droite);
+    }
+}
